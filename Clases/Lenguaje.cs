b7d3a1fde1e5ec3c7fc6e85805c861fa2b53dd5f
Agus <abarrosobol@frro.utn.edu.ar>
2026-02-08 20:03:51 -0300

    Proyecto Blazor simple con consumo de api y CRUD sin U

--- Clases/Lenguaje.cs
+++ Clases/Lenguaje.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace Clases
 {
     public class Lenguaje
     {
         public int? Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
         public List<Snippet?> Snippets { get; set; } = new();
 
--- Clases/Snippet.cs
+++ Clases/Snippet.cs
@@ -1,11 +1,15 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Clases
 {
     public class Snippet
     {
         public int? Id { get; set; }
+        [Required(ErrorMessage = "El titulo es obligatorio")]
         public string Titulo { get; set; }
         public int LenguajeId { get; set; }
+        [Required(ErrorMessage = "El código es obligatorio")]
         public string Codigo { get; set; }
     }
 
